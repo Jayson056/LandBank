c98bf4582c8b39f3e7600f35415cef3de7ef0195
Son <103288096+Jayson056@users.noreply.github.com>
2025-06-19 16:18:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 db_config = {
-    'host': 'dpg-d0sujere5dus73bk11d0-a', # e.g., 'dpg-abcdefg.oregon-postgres.render.com'
+    'host': 'dpg-d18mvf0gjchc739b8slg-a', # e.g., 'dpg-abcdefg.oregon-postgres.render.com'
     'port': 5432,                             # e.g., 5432
-    'database': 'landbank',        # e.g., 'landbank' or 'your_username'
-    'user': 'root',        # e.g., 'render_user'
-    'password': 'QkyqIQZkaTOEJDWHaHtOUudTxKyxIKr2'     # Your actual password
+    'database': 'database_landbank',        # e.g., 'landbank' or 'your_username'
+    'user': 'database_landbank_user',        # e.g., 'render_user'
+    'password': 'G7glMPn5QdMop7YdO6gLJZfoXX4o0vpe'     # Your actual password
 }
